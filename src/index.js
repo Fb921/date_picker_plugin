e97cf5518c566ef6fd4220cb0175e46fa7a20434
Bintou <fofana_bintou@outlook.fr>
2023-11-28 21:45:43 +0100

    Publish package

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import DatePicker from './DatePicker.jsx';
 
-export default DatePicker;
+module.exports = { DatePicker }
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
