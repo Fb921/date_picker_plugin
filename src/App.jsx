5e62c6ddafdffdf5ccf721cb36127b58c3ef2f64
Bintou <fofana_bintou@outlook.fr>
2023-12-04 02:20:19 +0100

    Debbugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import DatePicker from "./DatePicker.jsx"
 
+console.log('Nous sommes dans le App.js');
+
 function App(){
     return (<div><DatePicker />Hello there !</div>);
 }
