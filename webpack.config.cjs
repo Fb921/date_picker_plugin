0d21aaa1a6a9045ba1991678bea930423d6483f2
Bintou <fofana_bintou@outlook.fr>
2023-12-06 23:35:55 +0100

    Debbugging

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -6,7 +6,11 @@ module.exports =  {
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'index.bundle.js',
-        libraryTarget: "amd"
+        library: "DatePicker",
+        libraryTarget: "commonjs2",
+        // publicPath: '/dist/',      
+        // umdNamedDefine: true,  
+        clean: true,
     },
     devServer: {
         static: path.resolve(__dirname, 'build'),
@@ -51,9 +55,30 @@ module.exports =  {
                 ]
             }
         ]
-    },
+    }
+    ,
     experiments: {
         outputModule: true
-    }
+    },
+    // resolve: {      
+    //     alias: {          
+    //         'react': path.resolve(__dirname, './node_modules/react'),
+    //         'react-dom': path.resolve(__dirname, './node_modules/react-dom'),      
+    //     }  
+    // }, 
+    // externals: { 
+    //     react: {          
+    //         commonjs: "react",          
+    //         commonjs2: "react",          
+    //         amd: "React",          
+    //         root: "React"      
+    //     },      
+    //     "react-dom": {          
+    //         commonjs: "react-dom",          
+    //         commonjs2: "react-dom",          
+    //         amd: "ReactDOM",          
+    //         root: "ReactDOM"      
+    //     }  
+    // } 
 
 }
